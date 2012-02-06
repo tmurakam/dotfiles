f6862e24069750499a46c7205b3602fbfc8ef8a6
Takuya Murakami <murakami@da.jp.nec.com>
2012-02-06 17:03:15 +0900

    misc fix

--- emacs.d/init.d/ruby.el
+++ emacs.d/init.d/ruby.el
@@ -19,6 +19,8 @@
 	     (setq tab-width 8)))
 (setq ruby-indent-level 2)
 
+(setq auto-mode-alist (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+
 ;; rubydb
 (autoload 'rubydb "rubydb3x"
   "run rubydb on program file in buffer *gud-file*.
