83d9001f44e4436abceed9f4c0045218f9b45294
Takuya Murakami <murakami@da.jp.nec.com>
2012-02-06 16:53:14 +0900

    updated

--- emacs.d/init.d/01xwindow.el
+++ emacs.d/init.d/01xwindow.el
@@ -4,6 +4,7 @@
   (set-face-foreground 'modeline "yellow")
   (set-face-background 'modeline "navy")
   (set-scroll-bar-mode 'right)
+  (tool-bar-mode -1)
   (setq 
    ;;tool-bar-mode nil			;; Remove toolbar in Emacs-21
    default-frame-alist '(append
@@ -13,5 +14,4 @@
 			  ;;(height . 25)
 			  default-frame-alist)))))
 
-(tool-bar-mode -1)
 (menu-bar-mode -1)
