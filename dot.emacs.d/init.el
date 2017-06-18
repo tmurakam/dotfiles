68eef1424688fdb5b980732d47d53f865e050bbd
Takuya Murakami <tmurakam@tmurakam.org>
2017-06-19 00:09:25 +0900

    updated

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -47,3 +47,14 @@
 
 ;; other local settings
 (load "init_local")
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
