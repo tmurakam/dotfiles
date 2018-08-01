ae6c8b9a491991e7ff1a424d23d34c277852d394
Takuya Murakami <tmurakam@tmurakam.org>
2018-08-01 23:54:24 +0900

    rbenv

--- dot.bash_profile
+++ dot.bash_profile
@@ -38,8 +38,13 @@ fi
 . $HOME/dotfiles/init-rvm.sh
 
 # rbenv
-if [ -e $HOME/.rbenv ]; then
+if [ -d "$HOME/.rbenv/bin" ]; then
     export PATH=$HOME/.rbenv/bin:$PATH
+    eval "$(rbenv init -)"
+elif [ -d "/usr/local/rbenv" ]; then
+    export PATH=/usr/local/rbenv/bin:$PATH
+    eval "$(rbenv init -)"
+elif [ -x "/usr/local/bin/rbenv" ]; then # homebrew
     eval "$(rbenv init -)"
 fi
 
--- init-rvm.sh
+++ init-rvm.sh
@@ -6,15 +6,3 @@ elif [ -s /etc/profile.d/rvm.sh ]; then
 elif [ -s /usr/local/rvm/scripts/rvm ]; then
    source /usr/local/rvm/scripts/rvm
 fi
-
-# rbenv
-RBENV_ROOT=dummy
-if [ -d "$HOME/.rbenv/bin" ]; then
-    export RBENV_ROOT=$HOME/.rbenv
-elif [ -d "/usr/local/rbenv" ]; then
-    export RBENV_ROOT=/usr/local/rbenv
-fi
-if [ -d $RBENV_ROOT ]; then
-    export PATH=$RBENV_ROOT/bin:$PATH
-    eval "$(rbenv init -)"
-fi
